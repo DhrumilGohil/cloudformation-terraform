1d14bbbdc06a2329617e755929fb90218d7594d0
DhrumilGohil <dhrumilgohil17@gmail.com>
2024-03-19 00:50:14 +0530

    done

--- providers.tf
+++ providers.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region     = var.aws_region
 }
 terraform {
+   cloud {
+    organization = "dhrumil-test-org"
+
+    workspaces {
+      name = "test-macon"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
